ad06a459494b86766bdabf29de7748d746171d91
Reynaldo R <reynaldo.rodriguez@trainerroad.com>
2025-04-06 22:01:21 -0600

    lesson4pr.js

---
+++ JS Fundamentals/lessons/lesson4.js
@@ -0,0 +1,17 @@
+//Relational of comparison operator
+
+// > - more than
+// < - less than
+// >= - more than equal
+// <= - less than equal
+
+var result = 5 >= 5
+console.log(result)
+
+//equality operators
+
+var x = 1
+console.log(x == '1') // lose comparison
+console.log(x === '1') // string comparison
+
+
--- JS Fundamentals/lessons/lesson4pr.js
+++ JS Fundamentals/lessons/lesson4pr.js
@@ -1,1 +1,28 @@
-git
+
+const gym_member = {
+    memberName: 'Reynaldo',
+    age: 25, 
+    activeMembership: true,
+    listOfClasses: ["Math", "Geo", "Spanish", "Art"]
+}
+
+console.log(gym_member);
+
+let authorName = 'Reynaldo';
+console.log(`Welcome ${authorName}!`);
+
+let olderThan18 = gym_member.age >= 18;
+console.log(`Older than 18?: ${olderThan18}`);
+
+let activeMembership = true === gym_member.activeMembership;
+console.log('Active Membership?: '+activeMembership);
+
+let canEnterGym = olderThan18 == activeMembership;
+console.log(`Can enter gym: ${canEnterGym}`);
+
+let numberOfClasses = gym_member.listOfClasses.length;
+console.log(`Signed up for classes: ${numberOfClasses>0}`)
+
+
+
+
