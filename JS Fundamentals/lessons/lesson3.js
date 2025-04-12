e838eaafcd215a28c3a94cb51ff2bf0229d5b896
Reynaldo R <reynaldo.rodriguez@trainerroad.com>
2025-04-12 15:59:40 -0600

    --amend

--- JS Fundamentals/lessons/lesson3.js
+++ JS Fundamentals/lessons/lesson3.js
@@ -15,10 +15,19 @@ customer['lastName'] = 'Silver'
 console.log(customer['lastName']) //bracket + single/doble quotes
 
 console.log(`${customer.firstName} ${customer.lastName}`)
+console.log(customer.firstName+' '+customer.lastName)
+
+console.log() //Empty Line
 
 //arrays
 var car = ["volvo", 'Toyota', "Tesla"]
 car[1] = "BMW"
 console.log(car[1])
-console.log(customer.cars[0])
-console.log(customer['cars[0]'])
+console.log(customer.cars[0]) // Dot Notation
+console.log(customer['cars'][0]) // Bracket Notation
+
+
+console.log() //Empty Line
+
+console.log(`${customer.cars[0]} & ${customer.cars[1]}`)
+console.log(customer['cars'][0]+' & '+customer['cars'][1])
--- JS Fundamentals/lessons/lesson4pr.js
+++ JS Fundamentals/lessons/lesson4pr.js
@@ -23,6 +23,29 @@ console.log(`Can enter gym: ${canEnterGym}`);
 // 5. Check if signed
 let numberOfClasses = gym_member.listOfClasses.length;
 console.log(`Signed up for classes: ${numberOfClasses > 0}`);
+console.log()
+
+//NEW WAY: No variables in between
+console.log("NEW WAY: No variables in between");
+console.log();
+
+// 1. Welcome message using interpolation
+console.log('welcome '+gym_member["memberName"]+ "!");
+
+// 2. Check if member is 18 or older
+console.log(`Older than 18?: ${gym_member.age>=18}`);
+
+// 3. Check active membership
+console.log(`Active Membership?: ${gym_member.activeMembership}`);
+
+// 4. Can enter gym (must be adult AND active member)
+console.log(`Can enter gym: ${gym_member.age && gym_member.activeMembership}`);
+
+// 5. Check if signed
+console.log(`Signed up for classes: ${gym_member.listOfClasses.length>=0}`)
+
+console.log(`List of clases: ${gym_member.listOfClasses}`)
+console.log(`List of classes: ${gym_member.listOfClasses.join(', ')}`)
 
 
 
